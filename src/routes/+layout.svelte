6ad0ddab737ac7dad37c1c0b9a597187025f093d
feored <feordo@gmail.com>
2024-09-25 04:48:31 +0200

    add link to github

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script>
 	import '$lib/css/style.css';
 	import { version } from '$app/environment';
+	import { Github } from 'lucide-svelte';
 </script>
 
 <header class="my-4">
 	<h3>Trackmania Formatting Tool</h3>
 	<div style="text-align:right;">
 		<h6>v{version}</h6>
+		<a href="https://github.com/feored/tmformat/"
+			><button><Github size="1rem" /> Github</button>
+		</a>
 	</div>
 </header>
 <slot />
